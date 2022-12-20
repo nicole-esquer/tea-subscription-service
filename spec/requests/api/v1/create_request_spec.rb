1da96b3a9aae1dc3c199e2dad2b758e704013364
Nicole Esqier <nicole.esquer5@gmail.com>
2022-12-19 20:14:15 -0700

    testing: happy and sad path testing for create subscription complete and passing

--- spec/requests/api/v1/create_request_spec.rb
+++ spec/requests/api/v1/create_request_spec.rb
@@ -21,7 +21,44 @@ RSpec.describe 'Create new Subscriptions' do
       expect(response).to be_successful
       expect(response).to have_http_status(201)
 
-      new_subscription = JSON.parse(response.body, symbolize_names: true)
+      results = JSON.parse(response.body, symbolize_names: true)
+
+      expect(results).to have_key(:data)
+      expect(results[:data]).to have_key(:id)
+      expect(results[:data]).to have_key(:type)
+      expect(results[:data][:type]).to eq('subscription')
+
+      expect(results[:data]).to have_key(:attributes)
+      expect(results[:data][:attributes]).to have_key(:title)
+      expect(results[:data][:attributes][:title]).to eq('Flavorful, delicious teas of India')
+      expect(results[:data][:attributes]).to have_key(:price)
+      expect(results[:data][:attributes][:price]).to eq(15.5)
+      expect(results[:data][:attributes]).to have_key(:status)
+      expect(results[:data][:attributes][:status]).to eq('active')
+      expect(results[:data][:attributes]).to have_key(:frequency)
+      expect(results[:data][:attributes][:frequency]).to eq('quarterly')
+    end
+  end
+
+  context 'Sad Path' do
+    it 'will generate an error if there are missing params' do
+      customer = Customer.create!(first_name: 'Tea', last_name: 'Lover', email: 'tealover@mail.com',
+                                  address: '123 Boiling Kettle Lane, Tucson, AZ 85705')
+      tea = Tea.create!(title: 'Traditional Chai', description: 'Warm, rich spices from India', temperature: 100,
+                        brew_time: 3)
+
+      subscription_params = {
+        title: 'Flavorful, delicious teas of India',
+        price: '',
+        frequency: 3,
+        tea_id: tea.id
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post "/api/v1/#{customer.id}/subscriptions", headers: headers, params: JSON.generate(subscription_params)
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(400)
     end
   end
 end
