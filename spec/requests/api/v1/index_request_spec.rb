fcb61c1a5747e3de024ec1ea88dc892715521c9f
Nicole Esqier <nicole.esquer5@gmail.com>
2022-12-20 23:22:29 -0700

    feature: index for customer subscriptions complete, addtl testing needed

---
+++ spec/requests/api/v1/index_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Customer Subscription Index' do
+  context 'Happy Path' do
+    it 'returns all customer subscriptions both active and cancelled' do
+      customer = Customer.create!(first_name: 'Tea', last_name: 'Lover', email: 'tealover@mail.com',
+                                  address: '123 Boiling Kettle Lane, Tucson, AZ 85705')
+      tea = Tea.create!(title: 'Traditional Chai', description: 'Warm, rich spices from India', temperature: 100,
+                        brew_time: 3)
+      subscription = Subscription.create!(title: 'Flavorful, delicious teas of India', price: 15.50,
+                                          frequency: 3, tea_id: tea.id, customer_id: customer.id)
+
+      get "/api/v1/#{customer.id}/subscriptions"
+      results = JSON.parse(response.body, symbolize_names: true)
+
+      expect(results).to have_key(:data)
+    end
+  end
+end
