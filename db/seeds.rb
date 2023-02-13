ff748bd1c5a5c098e7d9c6c4eeb288cb453c60b8
Nicole Esqier <nicole.esquer5@gmail.com>
2023-02-13 10:25:01 -0700

    commented out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Subscription.destroy_all
+# Tea.destroy_all
+# Customer.destroy_all
+
+# customer_1 = Customer.create!(first_name: "Tea", last_name: "Lover", email: "tealover@mail.com", address: "123 Boiling Kettle Lane, Tucson, AZ 85705")
+# customer_2 = Customer.create!(first_name: "Belle", last_name: "Beauty", email: "belle_beauty@mail.com", address: "456 N Castle Drive, Tucson, AZ 85746")
+# customer_3 = Customer.create!(first_name: "Gaston", last_name: "Bully", email: "gastonbully@mail.com", address: "789 Crumble Cookies, Tucson, AZ 85718")
+# tea_1 = Tea.create!(title: "Chai", description: "Flavorful and delicious", temperature: 100, brew_time: "4 minutes")
+# tea_2 = Tea.create!(title: "Green Tea", description: "Energetic and classic", temperature: 101, brew_time: "3 minutes")
+# tea_3 = Tea.create!(title: "Jasmine", description: "Aromatic and pleasant", temperature: 102, brew_time: "2 minutes")
+# subscription_1 = customer_1.subscriptions.create!(title: "Charming Chai", price: 25.50, tea_id: tea_1.id, status: 0, frequency: 3)
+# subscription_2 = customer_1.subscriptions.create!(title: "Fresh Greens", price: 10.50, tea_id: tea_2.id, status: 1, frequency: 1)
+# subscription_3 = customer_1.subscriptions.create!(title: "Soothing Jasmine", price: 15.50, tea_id: tea_3.id, status: 0, frequency: 2)
